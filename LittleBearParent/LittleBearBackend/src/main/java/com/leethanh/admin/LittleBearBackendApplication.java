df63677aca7ee86fca82c874c125287f3da90050
ThanhBear <leethanh24003@gmail.com>
2024-01-22 07:43:12 +0700

    first commit

---
+++ LittleBearParent/LittleBearBackend/src/main/java/com/leethanh/admin/LittleBearBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leethanh.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleBearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleBearBackendApplication.class, args);
+	}
+
+}
---
+++ LittleBearParent/LittleBearFrontend/src/main/java/com/leethanh/site/LittleBearFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.leethanh.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleBearFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleBearFrontendApplication.class, args);
+	}
+
+}
