2e46e651ef07b0fc3640e8c138b3ee7054eeead1
ThanhBear <leethanh24003@gmail.com>
2024-01-31 11:29:12 +0700

    update soring

--- LittleBearParent/LittleBearBackend/src/main/java/com/leethanh/admin/user/UsersController.java
+++ LittleBearParent/LittleBearBackend/src/main/java/com/leethanh/admin/user/UsersController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -24,95 +26,105 @@ public class UsersController {
 
 	@Autowired
 	private UsersService usersService;
-	
 
-	
 	@GetMapping("/users")
-	public String listAll(Model model )
-	{
-		List<Users> listUsers = usersService.listAll();
-		model.addAttribute("listUsers",listUsers);
-		
-		return "users";
+	public void listAll(Model model) {
+		listUsersByPage(1, model, "id", "asc");
 	}
-	
+
 	@GetMapping("/users/new")
-	public String newUser( Model model)
-	{
+	public String newUser(Model model) {
 		List<Roles> listRoles = usersService.listRoles();
-		Users user= new Users();
+		Users user = new Users();
 		user.setEnabled(true);
-		System.out.println("user id == null ?"+( user.getId()==null));
-		model.addAttribute("user",user);
-		model.addAttribute("listRoles",listRoles);
-		model.addAttribute("pageTitle","Create New User");
+		System.out.println("user id == null ?" + (user.getId() == null));
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
 		return "user_form";
 	}
-	
+
 	@PostMapping("/users/save")
-	public String saveUser( Users user, @RequestParam ("image") MultipartFile multipartFile) throws IOException
-	{
-		
-		System.out.println("multipartfile is empty?"+multipartFile.isEmpty());
-		if (!multipartFile.isEmpty())
-		{
-			String fileName= StringUtils.cleanPath(multipartFile.getOriginalFilename());
-			System.out.println("photo name"+fileName);
+	public String saveUser(Users user, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+		System.out.println("multipartfile is empty?" + multipartFile.isEmpty());
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			System.out.println("photo name" + fileName);
 			user.setPhotos(fileName);
-			
+
 			Users savedUser = usersService.save(user);
-			String uploadDir= "user-photos/"+savedUser.getId();
+			String uploadDir = "user-photos/" + savedUser.getId();
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-			
-		}else 
-		{
-			System.out.println("user photo is empty ? :"+user.getPhotos().isEmpty());
-			if (user.getPhotos().isEmpty())
-			{
+
+		} else {
+			System.out.println("user photo is empty ? :" + user.getPhotos().isEmpty());
+			if (user.getPhotos().isEmpty()) {
 				user.setPhotos(null);
 			}
-			
+
 			Users savedUser = usersService.save(user);
-			
+
 		}
-		
-	
+
 		return "redirect:/users";
 	}
-	
+
 	@GetMapping("/users/edit/{id}")
-	public String editUser( @PathVariable ("id") Integer id,Model model,RedirectAttributes redirectAttributes)
-	{
+	public String editUser(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
 		try {
 			Users users = usersService.getUserById(id);
 			List<Roles> listRoles = usersService.listRoles();
-			
-			model.addAttribute("user",users);
-			model.addAttribute("id",id);
-			
-			model.addAttribute("listRoles",listRoles);
-			model.addAttribute("pageTitle","Edit User (ID - "+id+")");
+
+			model.addAttribute("user", users);
+			model.addAttribute("id", id);
+
+			model.addAttribute("listRoles", listRoles);
+			model.addAttribute("pageTitle", "Edit User (ID - " + id + ")");
 			return "user_form";
 		} catch (UserNotFoundException e) {
 			// TODO: handle exception
-			redirectAttributes.addFlashAttribute("message",e.getMessage());
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
 			return "redirect:/users";
 		}
-		
+
 	}
-	
-//	@GetMapping("/users/delete/{id}")
-//	public String deleteUser( @PathVariable ("id") Integer id,Model model,RedirectAttributes redirectAttributes)
-//	{
-//		try {
-//			usersService.delete(id);
-//			
-//		} catch (UserNotFoundException e) {
-//			// TODO: handle exception
-//			redirectAttributes.addFlashAttribute("message",e.getMessage());
-//		}
-//		return "redirect:/users";
-//
-//	}
+
+	@GetMapping("/users/page/{pageNum}")
+	public String listUsersByPage(@PathVariable("pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir) {
+		if (pageNum <= 0) {
+			pageNum = 1;
+		}
+		int lastPage = Math.round((float) (usersService.listAll().size() / (float) usersService.USER_PER_PAGE));
+		if (pageNum > lastPage) {
+			pageNum = lastPage;
+		}
+		Page<Users> page = usersService.listUsersByPage(pageNum, sortField, sortDir);
+		List<Users> listUsers = page.getContent();
+
+		int startCount = (pageNum - 1) * usersService.USER_PER_PAGE + 1;
+		int endCount = startCount + usersService.USER_PER_PAGE - 1;
+		int totalItem = usersService.listAll().size();
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		if (endCount > totalItem) {
+			endCount = totalItem;
+		}
+		System.out.println("reverse sort dir is :"+reverseSortDir);
+		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItem", totalItem);
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+
+		return "users";
+
+	}
 }
--- LittleBearParent/LittleBearBackend/src/test/java/com/leethanh/admin/user/UserRepositoryTests.java
+++ LittleBearParent/LittleBearBackend/src/test/java/com/leethanh/admin/user/UserRepositoryTests.java
@@ -2,6 +2,9 @@ package com.leethanh.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.booleanThat;
+import static org.mockito.ArgumentMatchers.intThat;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,120 +12,128 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
 
 import com.leethanh.common.entity.Roles;
 import com.leethanh.common.entity.Users;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
 public class UserRepositoryTests {
 
 	@Autowired
 	private UsersRepository usersRepository;
-	
+
 	@Autowired
 	private TestEntityManager entityManager;
-	
+
 	@Test
 	public void testCreateUserWithOneRole() {
 		Roles roleAdmin = entityManager.find(Roles.class, 1);
-		Users userThanhLe = new Users("lethanh@gmail.com","0204","Thanh","Le");
+		Users userThanhLe = new Users("lethanh@gmail.com", "0204", "Thanh", "Le");
 		userThanhLe.addRole(roleAdmin);
 		Users savedUser = usersRepository.save(userThanhLe);
 		assertThat(savedUser.getId()).isGreaterThan(0);
-		
+
 	}
-	
+
 	@Test
 	public void testCreateUserWithTwoRoles() {
-		Users userHoangChau = new Users("hc@gmail.com","0405","Chau","Pham");
+		Users userHoangChau = new Users("hc@gmail.com", "0405", "Chau", "Pham");
 		Roles roleEditor = entityManager.find(Roles.class, 4);
-		Roles roleAssistant = entityManager.find(Roles.class,3);
-		
+		Roles roleAssistant = entityManager.find(Roles.class, 3);
+
 		userHoangChau.addRole(roleEditor);
 		userHoangChau.addRole(roleAssistant);
-		
+
 		Users savedUser = usersRepository.save(userHoangChau);
 		assertThat(savedUser.getId()).isGreaterThan(0);
-		
+
 	}
-	
+
 	@Test
 	public void testListAllUsers() {
 		Iterable<Users> listUsersIterable = usersRepository.findAll();
-		listUsersIterable.forEach(user->System.out.println(user));
-		
-		
+		listUsersIterable.forEach(user -> System.out.println(user));
+
 	}
-	
+
 	@Test
-	public void testGetUserById () {
+	public void testGetUserById() {
 		Users userThanh = usersRepository.findById(1).get();
 		System.out.print(userThanh);
-		
+
 		assertThat(userThanh).isNotNull();
-		
-	
+
 	}
-	
+
 	@Test
-	public void testUpdateUserById () {
+	public void testUpdateUserById() {
 		Users userThanh = usersRepository.findById(1).get();
 		userThanh.setEnabled(true);
 		userThanh.setEmail("ledaoxuanthanh@gmail.com");
 		System.out.print(userThanh);
 		usersRepository.save(userThanh);
 		assertThat(userThanh).isNotNull();
-		
+
 	}
-	
+
 	@Test
-	public void testUpdateUserRoles () {
+	public void testUpdateUserRoles() {
 		Users userChau = usersRepository.findById(3).get();
 		Roles roleEditor = entityManager.find(Roles.class, 4);
 //		Roles roleSalePerson = entityManager.find(Roles.class,2);
-		
+
 		userChau.getRoles().remove(roleEditor);
 //		userChau.addRole(roleSalePerson);
-		
-		
+
 		usersRepository.save(userChau);
-		
-		
+
 	}
-	
+
 	@Test
-	public void testDeleteUser () {
-		Integer id = 3 ; 
+	public void testDeleteUser() {
+		Integer id = 3;
 		usersRepository.deleteById(id);
-		
-		
+
 	}
-	
+
 	@Test
-	public void testGetUserByEmail ()
-	{
-		String email= "ledaoxuanthanh@gmail.com";
+	public void testGetUserByEmail() {
+		String email = "ledaoxuanthanh@gmail.com";
 		Users user = usersRepository.getUsersByEmail(email);
 		assertThat(user).isNotNull();
 	}
-	
+
 	@Test
-	public void testCountById ()
-	{
+	public void testCountById() {
 		Integer id = 100;
 		Integer user = usersRepository.countById(id);
 		assertThat(user).isNotNull().isGreaterThan(0);
 	}
-	
+
 	@Test
-	public void testDisableUser ()
-	{
+	public void testDisableUser() {
 		Integer id = 1;
 		boolean enabled = false;
-		usersRepository.updateEnabledStatus(id,enabled);
+		usersRepository.updateEnabledStatus(id, enabled);
+
+	}
+
+	@Test
+	public void testListFirstPage() {
+		int pageNumber = 1;
+		int pageSize = 4;
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<Users> page = usersRepository.findAll(pageable);
 		
+		List<Users> listUsers = page.getContent();
+		listUsers.forEach(user->System.out.println(user));
+		
+
 	}
 }
